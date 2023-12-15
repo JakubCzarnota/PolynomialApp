e30aca99f81a8b45ffb1d4d782e305b295ab3db8
JakubCzarnota <86987378+JakubCzarnota@users.noreply.github.com>
2023-12-15 21:23:12 +0100

    Add tests for ExtremeValues and create Round method

--- PolynomialCore.Test/PolynomialTest.cs
+++ PolynomialCore.Test/PolynomialTest.cs
@@ -10,9 +10,16 @@ namespace PolynomialCore.Test
     {
         const int DECIMAL_PRECISION = 5;
 
-        [InlineData("3x+1", new double[] {1, 3}, 1)]
+        private static double Round(double x)
+        {
+            var number = Math.Pow(10, DECIMAL_PRECISION);
+
+            return Math.Round(x * number) / number;
+        }
+
+        [InlineData("3x+1", new double[] { 1, 3 }, 1)]
         [InlineData("5x^2+2x-10", new double[] { -10, 2, 5 }, 2)]
-        [InlineData("13x^3-20x+0", new double[] {0, -20, 0, 13}, 3)]
+        [InlineData("13x^3-20x+0", new double[] { 0, -20, 0, 13 }, 3)]
         [Theory]
         public void Constructor_ForGivenPolynomialFormlua_ReturnsCorrectPolynomial(string polynomialFormula, double[] coefficients, int degree)
         {
@@ -40,22 +47,20 @@ namespace PolynomialCore.Test
 
             var poly = new Polynomial(polynomialFormula);
 
-            var number = Math.Pow(10, DECIMAL_PRECISION);
-
             // act
 
-            var result = Math.Round(poly.Y(x) * number) / number;
+            var result = Round(poly.Y(x));
 
             // assert
 
-            result.Should().Be(Math.Round(y * number) / number);
+            result.Should().Be(Round(y));
         }
 
-        [InlineData("2x+10", new double[] {-5}, new int[] {1})]
+        [InlineData("2x+10", new double[] { -5 }, new int[] { 1 })]
         [InlineData("x^2-2x+1", new double[] { 1 }, new int[] { 2 })]
         [InlineData("x^2+1", new double[0], new int[0])]
-        [InlineData("x^3+4x^2-3x-18", new double[] {-3, 2}, new int[] {2, 1})]
-        [InlineData("x^4-x^3+2x-1", new double[] {-1.1537213755418, 0.5356873867919}, new int[] {1, 1})]
+        [InlineData("x^3+4x^2-3x-18", new double[] { -3, 2 }, new int[] { 2, 1 })]
+        [InlineData("x^4-x^3+2x-1", new double[] { -1.1537213755418, 0.5356873867919 }, new int[] { 1, 1 })]
         [Theory]
         public void FindRoots_ForGivenPolynomialFormlua_ReturnsCorrectRoots(string polynomialFormula, double[] rootsValue, int[] rootsMultiplicity)
         {
@@ -71,26 +76,56 @@ namespace PolynomialCore.Test
             poly.FindRoots();
 
             // assert
-            
+
             poly.Roots.Should().NotBeNull();
             poly.Roots!.Count.Should().Be(rootsValue.Length);
 
             for (int i = 0; i < poly.Roots.Count; i++)
             {
                 var root = poly.Roots![i];
-                root.Value = Math.Round(root.Value * DECIMAL_PRECISION) / DECIMAL_PRECISION;
 
                 var rootValue = rootsValue[i];
-                rootValue = Math.Round(rootValue * DECIMAL_PRECISION) / DECIMAL_PRECISION;
 
                 var rootMultiplicity = rootsMultiplicity[i];
 
-                root.Value.Should().Be(rootValue);
+                Round(root.Value).Should().Be(Round(rootValue));
                 root.Multiplicity.Should().Be(rootMultiplicity);
             }
 
 
         }
 
+        [InlineData("2x+10", new double[0], new double[0])]
+        [InlineData("x^2-2x-1", new double[] { 1 }, new double[] { -2 })]
+        [InlineData("x^3-1", new double[0], new double[0])]
+        [InlineData("x^3+4x^2-3x-18", new double[] { -3, 0.33333333333333 }, new double[] { 0, -18.5185185185185 })]
+        [Theory]
+        public void FindExtremeValues_ForGivenPolynomialFormlua_FindsCorrectExtremeValues(string polynomialFormula, double[] extremeValuesX, double[] extremeValuesY)
+        {
+            // arrange
+
+            var poly = new Polynomial(polynomialFormula);
+
+            var extremeValues = new List<Point>();
+
+            for (int i = 0; i < extremeValuesX.Length; i++)
+            {
+                extremeValues.Add(new Point(Round(extremeValuesX[i]), Round(extremeValuesY[i])));
+            }
+
+            // act
+
+            poly.FindExtremeValues();
+
+            for (int i = 0; i < poly.ExtremeValues!.Count; i++)
+            {
+                poly.ExtremeValues![i].X = Round(poly.ExtremeValues![i].X);
+                poly.ExtremeValues![i].Y = Round(poly.ExtremeValues![i].Y);
+            }
+
+            // assert
+
+            poly.ExtremeValues.Should().BeEquivalentTo(extremeValues);
+        }
     }
 }
