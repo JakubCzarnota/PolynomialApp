3196ce083b088ea3fd8af4a7b3c3e6f88aaa5635
JakubCzarnota <86987378+JakubCzarnota@users.noreply.github.com>
2023-12-15 16:22:13 +0100

    Add tests for FindRoots

--- PolynomialCore.Test/PolynomialTest.cs
+++ PolynomialCore.Test/PolynomialTest.cs
@@ -28,5 +28,48 @@ namespace PolynomialCore.Test
 
         }
 
+        const int DECIMAL_PRECISION = 5;
+
+        [InlineData("2x+10", new double[] {-5}, new int[] {1})]
+        [InlineData("x^2-2x+1", new double[] { 1 }, new int[] { 2 })]
+        [InlineData("x^2+1", new double[0], new int[0])]
+        [InlineData("x^3+4x^2-3x-18", new double[] {-3, 2}, new int[] {2, 1})]
+        [InlineData("x^4-x^3+2x-1", new double[] {-1.1537213755418, 0.5356873867919}, new int[] {1, 1})]
+        [Theory]
+        public void FindRoots_ForGivenPolynomialFormlua_ReturnsCorrectRoots(string polynomialFormula, double[] rootsValue, int[] rootsMultiplicity)
+        {
+            // arrange 
+
+            var number = Math.Pow(10, DECIMAL_PRECISION);
+
+            var poly = new Polynomial(polynomialFormula);
+
+
+            // act
+
+            poly.FindRoots();
+
+            // assert
+            
+            poly.Roots.Should().NotBeNull();
+            poly.Roots!.Count.Should().Be(rootsValue.Length);
+
+            for (int i = 0; i < poly.Roots.Count; i++)
+            {
+                var root = poly.Roots![i];
+                root.Value = Math.Round(root.Value * DECIMAL_PRECISION) / DECIMAL_PRECISION;
+
+                var rootValue = rootsValue[i];
+                rootValue = Math.Round(rootValue * DECIMAL_PRECISION) / DECIMAL_PRECISION;
+
+                var rootMultiplicity = rootsMultiplicity[i];
+
+                root.Value.Should().Be(rootValue);
+                root.Multiplicity.Should().Be(rootMultiplicity);
+            }
+
+
+        }
+
     }
 }
