e2bd3dcf1d23991ee6a95f55f8f055b0e2c8be61
JakubCzarnota <86987378+JakubCzarnota@users.noreply.github.com>
2023-12-18 14:38:41 +0100

    Add test for Polynomial operators

--- PolynomialCore.Test/PolynomialTest.cs
+++ PolynomialCore.Test/PolynomialTest.cs
@@ -30,7 +30,7 @@ namespace PolynomialCore.Test
         }
 
         [InlineData("25", new double[] { 25 }, 0)]
-        [InlineData("x", new double[] { 1, 0}, 1)]
+        [InlineData("x", new double[] { 1, 0 }, 1)]
         [InlineData("3x+1", new double[] { 1, 3 }, 1)]
         [InlineData("5x^2+2x-10", new double[] { -10, 2, 5 }, 2)]
         [InlineData("13x^3-20x+0", new double[] { 0, -20, 0, 13 }, 3)]
@@ -54,8 +54,8 @@ namespace PolynomialCore.Test
         public static IEnumerable<object[]> GetSampleDataForYTests()
         {
             yield return new object[] { new Polynomial("3x+1"), 2, 7 };
-            yield return new object[] { new Polynomial("5x^2+2x-10"), 1.5, 4.25};
-            yield return new object[] { new Polynomial("13x^3-20x+0"), 0.5, -8.375};
+            yield return new object[] { new Polynomial("5x^2+2x-10"), 1.5, 4.25 };
+            yield return new object[] { new Polynomial("13x^3-20x+0"), 0.5, -8.375 };
         }
 
         [MemberData(nameof(GetSampleDataForYTests))]
@@ -75,32 +75,32 @@ namespace PolynomialCore.Test
 
         public static IEnumerable<object[]> GetSampleDataForFindRootsTests()
         {
-            yield return new object[] { new Polynomial("2x+10"), 
-                new List<Root>() 
+            yield return new object[] { new Polynomial("2x+10"),
+                new List<Root>()
                 {
                     new Root(-5, 1)
                 }
             };
 
-            yield return new object[] { new Polynomial("x^2-2x+1"), 
-                new List<Root>() 
+            yield return new object[] { new Polynomial("x^2-2x+1"),
+                new List<Root>()
                 {
                     new Root(1, 2)
                 }
             };
 
-            yield return new object[] { new Polynomial("x^2+1"), new List<Root>() {}};
+            yield return new object[] { new Polynomial("x^2+1"), new List<Root>() { } };
 
-            yield return new object[] { new Polynomial("x^3+4x^2-3x-18"), 
-                new List<Root>() 
+            yield return new object[] { new Polynomial("x^3+4x^2-3x-18"),
+                new List<Root>()
                 {
                     new Root(-3, 2),
                     new Root(2, 1)
                 }
             };
 
-            yield return new object[] { new Polynomial("x^4-x^3+2x-1"), 
-                new List<Root>() 
+            yield return new object[] { new Polynomial("x^4-x^3+2x-1"),
+                new List<Root>()
                 {
                     new Root(-1.1537213755418, 1),
                     new Root(0.5356873867919, 1)
@@ -140,7 +140,7 @@ namespace PolynomialCore.Test
         {
             yield return new object[] { new Polynomial("2x+10"), new List<Point>() };
 
-            yield return new object[] { new Polynomial("x^2-2x-1"), 
+            yield return new object[] { new Polynomial("x^2-2x-1"),
                 new List<Point>()
                 {
                     new Point(1, -2)
@@ -149,7 +149,7 @@ namespace PolynomialCore.Test
 
             yield return new object[] { new Polynomial("x^3-1"), new List<Point>() };
 
-            yield return new object[] { new Polynomial("x^3+4x^2-3x-18"), 
+            yield return new object[] { new Polynomial("x^3+4x^2-3x-18"),
                 new List<Point>()
                 {
                     new Point(-3, 0),
@@ -189,7 +189,7 @@ namespace PolynomialCore.Test
 
         public static IEnumerable<object[]> GetSampleDataForFindMonotinicityTests()
         {
-            yield return new object[] { new Polynomial("2x+10"), 
+            yield return new object[] { new Polynomial("2x+10"),
                 (new List<Interval>()
                 {
                     new Interval(Interval.Infinity, Interval.Infinity, true)
@@ -197,7 +197,7 @@ namespace PolynomialCore.Test
                 new List<Interval>())
             };
 
-            yield return new object[] { new Polynomial("x^2-2x-1"), 
+            yield return new object[] { new Polynomial("x^2-2x-1"),
                 (new List<Interval>()
                 {
                     new Interval(1, Interval.Infinity, true)
@@ -208,7 +208,7 @@ namespace PolynomialCore.Test
                 })
             };
 
-            yield return new object[] { new Polynomial("x^3-1"), 
+            yield return new object[] { new Polynomial("x^3-1"),
                 (new List<Interval>()
                 {
                     new Interval(Interval.Infinity, Interval.Infinity, true)
@@ -216,7 +216,7 @@ namespace PolynomialCore.Test
                 new List<Interval>())
             };
 
-            yield return new object[] { new Polynomial("x^3+4x^2-3x-18"), 
+            yield return new object[] { new Polynomial("x^3+4x^2-3x-18"),
                 (new List<Interval>()
                 {
                     new Interval(Interval.Infinity, -3, true),
@@ -314,7 +314,7 @@ namespace PolynomialCore.Test
                 new List<Interval>()
                 {
                     new Interval(Interval.Infinity, -3),
-                    new Interval(-3, 2)            
+                    new Interval(-3, 2)
                 }
             };
         }
@@ -330,7 +330,7 @@ namespace PolynomialCore.Test
                 interval.A = Round(interval.A);
                 interval.B = Round(interval.B);
             }
-            
+
             foreach (var interval in negativeValues)
             {
                 interval.A = Round(interval.A);
@@ -404,7 +404,7 @@ namespace PolynomialCore.Test
                     new Point(7, 24),
                 }
             };
-            
+
             yield return new object[] { new Polynomial("x^2-2x-1"),
                 new List<Point>()
                 {
@@ -424,7 +424,7 @@ namespace PolynomialCore.Test
                     new Point(3, 26),
                 }
             };
-            
+
             yield return new object[] { new Polynomial("x^3+4x^2-3x-18"),
                 new List<Point>()
                 {
@@ -439,7 +439,7 @@ namespace PolynomialCore.Test
 
         [MemberData(nameof(GetSampleDataForGetPointsForGraphTests))]
         [Theory]
-        public void GetPointsForGraph_ReturnsCorrectPoints(Polynomial polynomial, List<Point> points) 
+        public void GetPointsForGraph_ReturnsCorrectPoints(Polynomial polynomial, List<Point> points)
         {
 
             // arrange
@@ -465,6 +465,143 @@ namespace PolynomialCore.Test
             result.Should().BeEquivalentTo(points);
 
         }
+
+        public static IEnumerable<object[]> GetSampleDataForOperatorsTests()
+        {
+            yield return new object[]
+            {
+                new Polynomial("x^2-2x-1"),
+                new Polynomial("2x+10"),
+                new Polynomial("x^2+9"),
+                Operators.add
+            };
+
+            yield return new object[]
+            {
+              new Polynomial("x^2-2x-1"),
+              new Polynomial("2x+10"),            
+              new Polynomial("x^2-4x-11"),
+              Operators.subtract
+            };
+
+            yield return new object[]
+            {
+                new Polynomial("x^2-2x-1"),
+                new Polynomial("2x+10"),
+                new Polynomial("2x^3+6x^2-22x-10"),
+                Operators.multiply
+            };
+
+            yield return new object[]
+            {
+                new Polynomial("x^2-2x-1"),
+                new Polynomial("2x+10"),
+                new Polynomial("0,5x-3,5"),
+                Operators.devide
+            };
+
+            yield return new object[]
+            {
+                new Polynomial("x^2-2x-1"),
+                new Polynomial("2x+10"),
+                new Polynomial("34"),
+                Operators.modulo
+            };
+
+            ///////////////////////////////
+
+            yield return new object[]
+            {
+                new Polynomial("x^3-1"),
+                new Polynomial("x^3+4x^2-3x-18"),
+                new Polynomial("2x^3+4x^2-3x-19"),
+                Operators.add
+            };
+
+            yield return new object[]
+            {
+              new Polynomial("x^3-1"),
+              new Polynomial("x^3+4x^2-3x-18"),
+              new Polynomial("-4x^2+3x+17"),
+              Operators.subtract
+            };
+
+            yield return new object[]
+            {
+                new Polynomial("x^3-1"),
+                new Polynomial("x^3+4x^2-3x-18"),
+                new Polynomial("x^6+4x^5-3x^4-19x^3-4x^2+3x+18"),
+                Operators.multiply
+            };
+
+            yield return new object[]
+            {
+                new Polynomial("x^3-1"),
+                new Polynomial("x^3+4x^2-3x-18"),
+                new Polynomial("1"),
+                Operators.devide
+            };
+
+            yield return new object[]
+            {
+                new Polynomial("x^3-1"),
+                new Polynomial("x^3+4x^2-3x-18"),
+                new Polynomial("-4x^2+3x+17"),
+                Operators.modulo
+            };
+
+        }
+
+        [MemberData(nameof(GetSampleDataForOperatorsTests))]
+        [Theory]
+        public void Operators_ForGivenTwoPolynomials_ReturnCorrectPolynomial(Polynomial a, Polynomial b, Polynomial resultPolynomial, Operators op)
+        {
+            // arange
+
+            var resultPolynomialCoefficients = resultPolynomial.Coefficients;
+
+            for (int i = 0; i < resultPolynomialCoefficients.Length; i++)
+            {
+                resultPolynomialCoefficients[i] = Round(resultPolynomialCoefficients[i]);
+            }
+
+            // act
+
+            Polynomial result;
+
+            switch (op)
+            {
+                case Operators.add:
+                    result = (a + b);
+                    break;
+                case Operators.subtract:
+                    result = a - b;
+                    break;
+                case Operators.multiply:
+                    result = a * b;
+                    break;
+                case Operators.devide:
+                    result = a / b;
+                    break;
+                case Operators.modulo:
+                    result = a % b;
+                    break;
+                default:
+                    throw new InvalidEnumArgumentException();
+            }
+
+            var resultCoefficients = result.Coefficients;
+
+            for (int i = 0; i < resultCoefficients.Length; i++)
+            {
+                resultCoefficients[i] = Round(resultCoefficients[i]);
+            }
+
+            // assert 
+
+            resultCoefficients.Should().BeEquivalentTo(resultPolynomialCoefficients);
+        }
+
 
     }
 }
