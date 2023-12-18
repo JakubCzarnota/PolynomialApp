f1998fe30a65b3bb0adb15d8ef1a90d70f249eae
JakubCzarnota <86987378+JakubCzarnota@users.noreply.github.com>
2023-12-18 12:12:18 +0100

    Add more tests for Pomynomial constructor

---
+++ PolynomialCore.Test/Operators.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolynomialCore.Test
+{
+    public enum Operators
+    {
+        add,
+        subtract,
+        multiply,
+        devide,
+        modulo
+    }
+}
