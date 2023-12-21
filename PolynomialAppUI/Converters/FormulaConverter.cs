14958a201d98a7fef82aae5d1c17dd8300f4aa27
JakubCzarnota <86987378+JakubCzarnota@users.noreply.github.com>
2023-12-21 12:39:25 +0100

    Add TextBlocks with polynomial's formulas

---
+++ PolynomialAppUI/Converters/FormulaConverter.cs
@@ -0,0 +1,38 @@
+﻿using PolynomialCore;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PolynomialAppUI.Converters
+{
+    class FormulaConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var poly = value as Polynomial;
+
+            if (poly == null)
+                return "";
+
+            var formulaType = (FormulaTypes)Enum.Parse(typeof(FormulaTypes), parameter.ToString()!);
+
+            try
+            {
+                return poly.ToString(formulaType);
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null!;
+        }
+    }
+}
